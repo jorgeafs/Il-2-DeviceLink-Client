6fee1bde0afd6a02040c41a816ddc91e6e1e5107
Jorge <Jorge@192.168.1.33>
2015-07-06 17:05:41 +0200

    Divido la carga en cliente y server

---
+++ Il 2 DeviceLink Client/deviceLink/udp/UDPServer.java
@@ -0,0 +1,14 @@
+package udp;
+
+/**UDPserverClient:
+ * Da soporte UDP al resto del programa. Su unica funcion es mandar y recibir paquetes UDP
+ * 
+ * Propiedades:
+ * 		socket --> basica, entero, modificable y consultable
+ * 
+ */
+
+import java.net.*;
+public class UDPServer {
+
+}
